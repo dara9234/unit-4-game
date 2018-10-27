d6b94659bc72db7c993f99f116bb2bb2568f8f23
Dara <dahmed@alumni.depaul.edu>
2018-10-27 18:57:05 -0500

    Assignment 4 Crystal Collector Dara

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # unit-4-game
 Assignment 4 Crystals Collector Game (Recommended)
+
+Pseducode:
+There are four crystals displayed as buttons on the page.
+The player will be shown a random number at the start of the game.
+
+When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+
+
+Then the game will hide this amount until the player clicks a crystal.
+When they do click one, It will update the player's score counter.
+
+
+The player wins if their total score matches the random number from the beginning of the game.
+
+The player loses if their score goes above the random number.
+
+The game restarts whenever the player wins or loses.
+
+
+When the game begins again, the player sees a new random number. 
+
+Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+
+The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+The random number shown at the start of the game is between 19 - 120.
+Each crystal has a random hidden value between 1 - 12.
+*********************************************************************
+
+
+1.
+
+
+
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,136 @@
+// Computer generated random number given to the player
+var computerPickNum= Math.floor(Math.random()* 101)+19;
+
+//assigning all four crsytal variables(ruby,diamond,topaz and emerald) to random numbers
+ var ruby  =  Math.floor(Math.random()*12)+1;
+ var diamond = Math.floor(Math.random()*12)+1;
+ var topaz = Math.floor(Math.random()*12)+1;
+ var emerald= Math.floor(Math.random()*12)+1;
+
+//setting player total score variable to 0 
+var playerTotalScore=0;
+
+//setting player's wins and losses variables to 0
+var wins=0;
+var losses=0;
+
+
+$(document).ready(function(){
+
+
+ function initial()
+ {
+    $("#random-area").html(computerPickNum);
+    // $("#score-area").html(playerTotalScore);
+ }
+function resetGame()
+{
+     //resetting all crystal variables and computerpick to new random picks
+       computerPickNum= Math.floor(Math.random()* 101)+19;
+       ruby  =  Math.floor(Math.random()*12)+1;
+       diamond = Math.floor(Math.random()*12)+1;
+       topaz = Math.floor(Math.random()*12)+1;
+       emerald= Math.floor(Math.random()*12)+1;
+
+    //resetting player total to zero
+     playerTotalScore=0;
+    playGame();
+
+    $("#score-area").html("");
+    $("#random-area").html(computerPickNum);
+       
+
+}
+function playGame(){
+ 
+    $("#diamond").on("click",function(){
+    //  global variable diamond that the set random
+        //totaluser+=global variable diamond
+                //score-title div to display totaluser
+
+        playerTotalScore+=diamond;
+        $("#score-area").html( playerTotalScore);
+        checkWinStatus();
+        
+       
+    });
+
+    $("#emerald").on("click",function(){
+//  global variable emerald that the set random
+        //totaluser+=global variable emerald
+                //score-title div to display totaluser
+
+        playerTotalScore+=emerald;
+       
+        $("#score-area").html( playerTotalScore); 
+        checkWinStatus();
+       
+      
+    });
+
+    $("#topaz").on("click",function(){
+//  global variable topaz that the set random
+        //totaluser+=global variable topaz
+           //score-title div to display totaluser
+
+        playerTotalScore+=topaz;
+       
+        $("#score-area").html( playerTotalScore); 
+        checkWinStatus();
+      
+      
+        
+    });
+
+    $("#ruby").on("click",function(){
+//  global variable ruby that the set random
+        //totaluser+=global variable ruby
+        //score-title div to display totaluser
+
+        playerTotalScore+=ruby;
+       
+        $("#score-area").html( playerTotalScore);
+        checkWinStatus();
+       
+       
+        
+    });
+}
+function checkWinStatus() {
+
+       if(playerTotalScore===computerPickNum) {
+        wins++;
+        $("#win-area").html("You WON!  "+ "<br> Wins: "+ wins); //displaying win-area div that displays You're a winner!
+        
+        resetGame(); //calling reset game function
+    }
+      else if(playerTotalScore>computerPickNum) {
+         losses++;
+         $("#win-area").html("You Lost! "+  "<br> Losses: "+losses);    //displaying win-area div that displays You're a Loser!
+        
+         resetGame(); // calling reset game function
+     }
+}
+
+initial();
+
+playGame();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
